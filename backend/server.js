dcd15f963ff2a0ba03569a4876e6e1beca8e539e
jibaresucm <javier.ibares1@gmail.com>
2026-01-18 17:52:30 +0100

    Notas

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const app = express()
 
 const PORT = 47321
 
+app.use(express.json())
+
+app.get("*", (res, rep) =>{
+    
+})
 
 app.listen(port= PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
