96f5db430a2a1aab52df7acca191a501df35fecd
jibaresucm <javier.ibares1@gmail.com>
2026-01-18 18:36:01 +0100

    REact working with express port 5050

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const PORT = 5050
 
 app.use(express.json())
-
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get("/", (req, res) => {
   res.sendFile(
-    path.join(__dirname, '/../frontend/index.html')
+    path.join(__dirname, '/../frontend/dist/index.html')
   );
 });
 
