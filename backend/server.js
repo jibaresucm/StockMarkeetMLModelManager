f9dbe45ea683be5de681e369c055aa5581c01390
jibaresucm <javier.ibares1@gmail.com>
2026-01-18 18:15:28 +0100

    Vite React

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
-const PORT = 47321
+const PORT = 5050
 
 app.use(express.json())
 
-app.get("*", (res, rep) =>{
-    
-})
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, '/../frontend/index.html')
+  );
+});
 
 app.listen(port= PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
