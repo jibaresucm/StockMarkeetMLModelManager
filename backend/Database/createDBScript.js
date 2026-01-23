92bad915b242005b338482fabfb2e6c5a99376ed
huawu04 <82873950+huawu04@users.noreply.github.com>
2026-01-23 10:23:57 +0100

    Revert "creation of the DB for projects and models along with the update for the script in the dbscript and changes to the front end models and projects .jsx"
    This reverts commit f8d6fec58e8c28dfd8a643c46f41e8319dfdcfe6.

--- backend/Database/createDBScript.js
+++ backend/Database/createDBScript.js
@@ -15,22 +15,3 @@ db.execute(`CREATE TABLE IF NOT EXISTS users (
     password_hash VARCHAR(255) NOT NULL, 
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 );`)
-
-db.execute(`CREATE TABLE IF NOT EXISTS projects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL ,
-    name VARCHAR(100) NOT NULL ,
-    description TEXT, 
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-);`)
-
-db.execute(`CREATE TABLE IF NOT EXISTS models (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    project_id INT NOT NULL ,
-    name VARCHAR(100) NOT NULL ,
-    description TEXT, 
-    version VARCHAR(20) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
-);`)
