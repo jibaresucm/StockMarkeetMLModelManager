2cc3933fd964b9b10c095b1f7d1636a81a7bac11
huawu04 <82873950+huawu04@users.noreply.github.com>
2026-04-16 23:47:59 +0200

    [FIX]: minor changes  and added gitignore

--- backend/Routers/api.js
+++ backend/Routers/api.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const models = require('./ApiRouters/modelApi.js')
 const projects = require('./ApiRouters/proyectApi.js')
-const newsApi = require("./ApiRouter/newsApi.js")
 
 
 const api = express.Router()
 
 api.use('/models', models)
 api.use('/projects', projects)
-api.use('/news', newsApi)
 
 module.exports = api
