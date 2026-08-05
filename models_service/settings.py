c1d0cd4eeed8bcba4338a796c60146a6a7787c3d
jibaresucm <javier.ibares1@gmail.com>
2026-08-05 13:12:26 +0200

    fix

---
+++ models_service/settings.py
@@ -0,0 +1,13 @@
+import os
+
+class Settings():
+    #MySQL settings
+    MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+    MYSQL_DB = os.environ.get("MYSQL_DB","news_db")
+    MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "password")
+    
+    #Ollama
+    OLLAMA_HOST = os.environ.get("OLLAMA_HOST", "localhost")
+    OLLAMA_PORT = os.environ.get("OLLAMA_PORT", 11434)
+    OLLAMA_MODEL = "llama3.2"
